b1cf6be8d7a21b06dbe474a9be8b6dd660565fa5
tayne3 <tayne3@dingtalk.com>
2024-11-27 19:07:53 +0800

    style: 🎨 解决 部分宏定义存在 命名冲突的风险

--- src/mech/ct_log.h
+++ src/mech/ct_log.h
@@ -14,65 +14,65 @@ extern "C" {
 #include "log/ct_logger.h"
 #include "mech/ct_threadcache.h"
 
-#define CTLogger_HandleBasic(__flag, __type, ...)                                                        \
-	do {                                                                                                 \
-		if (ct_log_is_enable(__type, CTLog_Level##__flag)) {                                             \
-			ct_threadcache_t *cache = ct_threadcache_get();                                              \
-			const int         _size = __ct_threadcache_basic(cache, __VA_ARGS__);                        \
-			ct_log_handle(__type, CTLog_Level##__flag, ct_threadcache_get_buffer(cache), (size_t)_size); \
-		}                                                                                                \
+#define CTLogger_HandleBasic(__flag, __type, ...)                                                           \
+	do {                                                                                                    \
+		if (ct_log_is_enable(__type, CTLog_Level##__flag)) {                                                \
+			ct_threadcache_t *__cache = ct_threadcache_get();                                               \
+			const int         __size  = __ct_threadcache_basic(__cache, __VA_ARGS__);                       \
+			ct_log_handle(__type, CTLog_Level##__flag, ct_threadcache_get_buffer(__cache), (size_t)__size); \
+		}                                                                                                   \
 	} while (0)
 
-#define CTLogger_HandleBrief(__flag, __type, ...)                                                                      \
-	do {                                                                                                               \
-		if (ct_log_is_enable(__type, CTLog_Level##__flag)) {                                                           \
-			ct_threadcache_t *cache = ct_threadcache_get();                                                            \
-			const int         _size =                                                                                  \
-				__ct_threadcache_brief(cache, CTLog_Style##__flag CTLog_String##__flag "|%s|%s\x1b[0m ", __VA_ARGS__); \
-			ct_log_handle(__type, CTLog_Level##__flag, ct_threadcache_get_buffer(cache), (size_t)_size);               \
-		}                                                                                                              \
+#define CTLogger_HandleBrief(__flag, __type, ...)                                                           \
+	do {                                                                                                    \
+		if (ct_log_is_enable(__type, CTLog_Level##__flag)) {                                                \
+			ct_threadcache_t *__cache = ct_threadcache_get();                                               \
+			const int         __size  = __ct_threadcache_brief(                                             \
+                __cache, CTLog_Style##__flag CTLog_String##__flag "|%s|%s\x1b[0m ", __VA_ARGS__);  \
+			ct_log_handle(__type, CTLog_Level##__flag, ct_threadcache_get_buffer(__cache), (size_t)__size); \
+		}                                                                                                   \
 	} while (0)
 
-#define CTLogger_HandleDetail(__flag, __type, ...)                                                         \
-	do {                                                                                                   \
-		if (ct_log_is_enable(__type, CTLog_Level##__flag)) {                                               \
-			ct_threadcache_t *cache = ct_threadcache_get();                                                \
-			const int         _size = __ct_threadcache_detail(                                             \
-                cache, STR_SEPARATOR __ct_file__, __ct_line__,                                     \
+#define CTLogger_HandleDetail(__flag, __type, ...)                                                          \
+	do {                                                                                                    \
+		if (ct_log_is_enable(__type, CTLog_Level##__flag)) {                                                \
+			ct_threadcache_t *__cache = ct_threadcache_get();                                               \
+			const int         __size  = __ct_threadcache_detail(                                            \
+                __cache, STR_SEPARATOR __ct_file__, __ct_line__,                                   \
                 CTLog_Style##__flag CTLog_String##__flag "|%s|%s\x1b[0m [%.*s:%d] ", __VA_ARGS__); \
-			ct_log_handle(__type, CTLog_Level##__flag, ct_threadcache_get_buffer(cache), (size_t)_size);   \
-		}                                                                                                  \
+			ct_log_handle(__type, CTLog_Level##__flag, ct_threadcache_get_buffer(__cache), (size_t)__size); \
+		}                                                                                                   \
 	} while (0)
 
-#define CTLogger_HandleHex(__flag, __type, __buf, __len)                                 \
-	do {                                                                                 \
-		if ((__len) > 0 && ct_log_is_enable(__type, CTLog_Level##__flag)) {              \
-			ct_threadcache_t *cache      = ct_threadcache_get();                         \
-			char             *_buffer    = ct_threadcache_get_buffer(cache);             \
-			char             *_dst       = _buffer;                                      \
-			const uint8_t    *_src       = (const uint8_t *)(__buf);                     \
-			size_t            _available = 1024;                                         \
-			const char       *hex_table  = "0123456789ABCDEF";                           \
-			for (size_t _i = 0; _i < (size_t)(__len); _i++) {                            \
-				if (_available < 3) {                                                    \
-					ct_log_handle(__type, CTLog_Level##__flag, _buffer, _dst - _buffer); \
-					_dst       = _buffer;                                                \
-					_available = 1024;                                                   \
-				}                                                                        \
-				const uint8_t byte = _src[_i];                                           \
-				*_dst++            = hex_table[byte >> 4];                               \
-				*_dst++            = hex_table[byte & 0x0F];                             \
-				if (_i != (size_t)(__len) - 1) {                                         \
-					*_dst++ = ' ';                                                       \
-					_available -= 3;                                                     \
-				} else {                                                                 \
-					_available -= 2;                                                     \
-				}                                                                        \
-			}                                                                            \
-			if (_dst > _buffer) {                                                        \
-				ct_log_handle(__type, CTLog_Level##__flag, _buffer, _dst - _buffer);     \
-			}                                                                            \
-		}                                                                                \
+#define CTLogger_HandleHex(__flag, __type, __buf, __len)                                    \
+	do {                                                                                    \
+		if ((__len) > 0 && ct_log_is_enable(__type, CTLog_Level##__flag)) {                 \
+			ct_threadcache_t *__cache     = ct_threadcache_get();                           \
+			char             *__buffer    = ct_threadcache_get_buffer(__cache);             \
+			char             *__dst       = __buffer;                                       \
+			const uint8_t    *__src       = (const uint8_t *)(__buf);                       \
+			size_t            __available = 1024;                                           \
+			const char       *__hex_table = "0123456789ABCDEF";                             \
+			for (size_t __i = 0; __i < (size_t)(__len); __i++) {                            \
+				if (__available < 3) {                                                      \
+					ct_log_handle(__type, CTLog_Level##__flag, __buffer, __dst - __buffer); \
+					__dst       = __buffer;                                                 \
+					__available = 1024;                                                     \
+				}                                                                           \
+				const uint8_t byte = __src[__i];                                            \
+				*__dst++           = __hex_table[byte >> 4];                                \
+				*__dst++           = __hex_table[byte & 0x0F];                              \
+				if (__i != (size_t)(__len) - 1) {                                           \
+					*__dst++ = ' ';                                                         \
+					__available -= 3;                                                       \
+				} else {                                                                    \
+					__available -= 2;                                                       \
+				}                                                                           \
+			}                                                                               \
+			if (__dst > __buffer) {                                                         \
+				ct_log_handle(__type, CTLog_Level##__flag, __buffer, __dst - __buffer);     \
+			}                                                                               \
+		}                                                                                   \
 	} while (0)
 
 #ifdef __cplusplus
