44462ed9c59610ce4094b9e8ebced12c588ec599
tayne3 <tayne3@dingtalk.com>
2025-06-03 15:55:48 +0800

    build: 🛠️ 更新 CMake 配置以增强编译选项管理
    - 移除 CompileOptions.cmake 文件，整合编译选项至 ProjectConfig.cmake
    - 引入 coter_compile_dependency 库以集中管理编译依赖和选项
    - 调整 CMakeLists.txt，更新库链接方式，确保编译选项的正确性
    - 调整 example/CMakeLists.txt，移除不再需要的编译选项引用

--- cmake/ProjectConfig.cmake
+++ cmake/ProjectConfig.cmake
@@ -1,14 +1,16 @@
+add_library(coter_compile_dependency INTERFACE)
+target_compile_features(coter_compile_dependency INTERFACE c_std_99)
+
 if(NOT DEFINED ENV{CPM_SOURCE_CACHE})
-  set(ENV{CPM_SOURCE_CACHE} ${CMAKE_SOURCE_DIR}/cmake_modules)
+	set(ENV{CPM_SOURCE_CACHE} ${CMAKE_SOURCE_DIR}/cmake_modules)
 endif()
 string(TOLOWER "${CMAKE_SYSTEM_NAME}" SYSTEM_NAME)
-
 if(COTER_BUILD_DEBUG)
 	set(COTER_OUTPUT_DIR ${PROJECT_SOURCE_DIR}/bin/debug-${SYSTEM_NAME})
-    set(COTER_COMPILE_DEFINITIONS -DDEBUG)
+	target_compile_definitions(coter_compile_dependency INTERFACE -DDEBUG)
 else()
 	set(COTER_OUTPUT_DIR ${PROJECT_SOURCE_DIR}/bin/release-${SYSTEM_NAME})
-	set(COTER_COMPILE_DEFINITIONS -DNDEBUG)
+	target_compile_definitions(coter_compile_dependency INTERFACE -DNDEBUG)
 endif()
 
 if(NOT CMAKE_RUNTIME_OUTPUT_DIRECTORY)
@@ -17,9 +19,9 @@ endif()
 if(NOT CMAKE_LIBRARY_OUTPUT_DIRECTORY)
 	set(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${COTER_OUTPUT_DIR})					
 endif()
-if(NOT CMAKE_ARCHIVE_OUTPUT_DIRECTORY)
-	set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY ${COTER_OUTPUT_DIR})					
-endif()
+# if(NOT CMAKE_ARCHIVE_OUTPUT_DIRECTORY)
+# 	set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY ${COTER_OUTPUT_DIR})					
+# endif()
 
 include(GNUInstallDirs)
 set(CMAKE_SKIP_BUILD_RPATH OFF)
@@ -49,3 +51,28 @@ set(CMAKE_CXX_EXTENSIONS ON)
 set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
 set(CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS ON)
 set(CMAKE_POSITION_INDEPENDENT_CODE ON)
+
+# 为MSVC编译器设置特定的C语言选项
+if(CMAKE_C_COMPILER_ID STREQUAL "MSVC")
+    target_compile_options(coter_compile_dependency INTERFACE 
+		"$<$<COMPILE_LANGUAGE:C>:/source-charset:utf-8>"
+	)
+endif()
+
+# 为非MSVC和非ClangCL（MSVC前端）的编译器设置C语言警告选项
+# 这些选项有助于捕捉潜在的代码问题
+if(NOT (CMAKE_C_COMPILER_ID STREQUAL "MSVC" OR ("${CMAKE_C_COMPILER_ID}" STREQUAL "Clang" AND "${CMAKE_C_COMPILER_FRONTEND_VARIANT}" STREQUAL "MSVC")))
+    target_compile_options(coter_compile_dependency INTERFACE
+        "$<$<COMPILE_LANGUAGE:C>:-Wall>"
+        "$<$<COMPILE_LANGUAGE:C>:-Wextra>"
+        "$<$<COMPILE_LANGUAGE:C>:-Werror=return-type>"
+    )
+endif()
+
+# 处理旧版本GCC（低于6.0）的特定警告抑制选项
+# -Wno-missing-field-initializers 用于抑制因结构体未完全初始化而产生的警告
+if(CMAKE_C_COMPILER_ID STREQUAL "GNU" AND CMAKE_C_COMPILER_VERSION VERSION_LESS 6.0)
+    target_compile_options(coter_compile_dependency INTERFACE 
+		"$<$<COMPILE_LANGUAGE:C>:-Wno-missing-field-initializers>"
+	)
+endif()
