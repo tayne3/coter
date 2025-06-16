10218ce1b1c940c7352666066a9797b2b916b74f
tayne3 <tayne3@dingtalk.com>
2025-06-16 17:26:33 +0800

    build: 🛠️ 完善 CMake 配置，使用更加统一规范的变量名
    - 更新 CMakeLists.txt，使用 PROJECT_NAME 变量替代 COTER 相关变量，增强可读性和一致性
    - 调整 OptionVariables.cmake，统一管理库类型和名称
    - 引入 PROJECT_BUILD_DEBUG 标志，优化调试构建设置

--- cmake/OptionVariables.cmake
+++ cmake/OptionVariables.cmake
@@ -10,7 +10,6 @@
 # | Variable / Option         | Availability        | Default                                       | Description                                                   |
 # |===========================|=====================|===============================================|===============================================================|
 # | CMAKE_BUILD_TYPE          | Always              | "Debug" (if not set)                          | Standard CMake: Debug, Release, MinSizeRel, RelWithDebInfo.   |
-# | COTER_BUILD_DEBUG         | Always (Derived)    | ON (for Debug, RelWithDebInfo), OFF otherwise | Internal flag for debug builds.                               |
 # | COTER_BUILD_SHARED        | Always (Option)     | OFF                                           | Build shared libraries if ON, static if OFF.                  |
 # | COTER_BUILD_EXAMPLE       | Top-Level (Option)  | OFF                                           | Build example programs.                                       |
 # | COTER_BUILD_TEST          | Top-Level (Option)  | OFF                                           | Build test programs.                                          |
@@ -27,7 +26,6 @@
 # | Variable / Option         | Availability        | Default                                       | Description                                                   |
 # |===========================|=====================|===============================================|===============================================================|
 # | CMAKE_BUILD_TYPE          | 总是                | "Debug" (如果未设置)                          | 标准 CMake 变量：Debug, Release, MinSizeRel, RelWithDebInfo。 |
-# | COTER_BUILD_DEBUG         | 总是 (派生)         | ON (对应 Debug, RelWithDebInfo), OFF (其他)   | 内部标志，用于调试构建。                                      |
 # | COTER_BUILD_SHARED        | 总是 (选项)         | OFF                                           | 如果为 ON 构建共享库，为 OFF 构建静态库。                     |
 # | COTER_BUILD_EXAMPLE       | 顶层项目 (选项)     | OFF                                           | 构建示例程序。                                                |
 # | COTER_BUILD_TEST          | 顶层项目 (选项)     | OFF                                           | 构建测试程序。                                                |
@@ -40,10 +38,10 @@ if(NOT CMAKE_BUILD_TYPE)
 endif()
 if(("${CMAKE_BUILD_TYPE}" STREQUAL "Debug") # For debugging purposes, includes symbol table and unoptimized code (-O0 -g)
 	OR ("${CMAKE_BUILD_TYPE}" STREQUAL "RelWithDebInfo")) # Includes optimized code and symbol table for debugging when errors occur (-O2 -g -DNDEBUG)
-	set(COTER_BUILD_DEBUG ON)
+	set(PROJECT_BUILD_DEBUG ON)
 elseif(("${CMAKE_BUILD_TYPE}" STREQUAL "Release") # For release purposes, includes optimized code and typically does not include symbol table (-O3 -DNDEBUG)
 	OR ("${CMAKE_BUILD_TYPE}" STREQUAL "MinSizeRel")) # Minimizes binary size, enables optimizations, and removes unnecessary symbols (-Os -DNDEBUG)
-	set(COTER_BUILD_DEBUG OFF)
+	set(PROJECT_BUILD_DEBUG OFF)
 else()
 	message(FATAL_ERROR "Unsupported build type: ${CMAKE_BUILD_TYPE}.")
 endif()
@@ -51,11 +49,11 @@ endif()
 option(COTER_BUILD_SHARED "build shared library" OFF)
 mark_as_advanced(COTER_BUILD_SHARED)
 if(COTER_BUILD_SHARED)
-    set(COTER_LIB_TYPE SHARED)
-    set(COTER_LIB_NAME shared)
+    set(PROJECT_LIB_TYPE SHARED)
+    set(PROJECT_LIB_NAME shared)
 else()
-    set(COTER_LIB_TYPE STATIC)
-    set(COTER_LIB_NAME static)
+    set(PROJECT_LIB_TYPE STATIC)
+    set(PROJECT_LIB_NAME static)
 endif()
 
 if(PROJECT_IS_TOP_LEVEL)
--- cmake/ProjectConfig.cmake
+++ cmake/ProjectConfig.cmake
@@ -1,27 +1,12 @@
-add_library(coter_compile_dependency INTERFACE)
-target_compile_features(coter_compile_dependency INTERFACE c_std_99)
-
-if(NOT DEFINED ENV{CPM_SOURCE_CACHE})
-	set(ENV{CPM_SOURCE_CACHE} ${CMAKE_SOURCE_DIR}/cmake_modules)
-endif()
-string(TOLOWER "${CMAKE_SYSTEM_NAME}" SYSTEM_NAME)
-if(COTER_BUILD_DEBUG)
-	set(COTER_OUTPUT_DIR ${PROJECT_SOURCE_DIR}/bin/debug-${SYSTEM_NAME})
-	target_compile_definitions(coter_compile_dependency INTERFACE -DDEBUG)
-else()
-	set(COTER_OUTPUT_DIR ${PROJECT_SOURCE_DIR}/bin/release-${SYSTEM_NAME})
-	target_compile_definitions(coter_compile_dependency INTERFACE -DNDEBUG)
-endif()
-
-if(NOT CMAKE_RUNTIME_OUTPUT_DIRECTORY)
-	set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${COTER_OUTPUT_DIR})					
-endif()
-if(NOT CMAKE_LIBRARY_OUTPUT_DIRECTORY)
-	set(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${COTER_OUTPUT_DIR})					
-endif()
-# if(NOT CMAKE_ARCHIVE_OUTPUT_DIRECTORY)
-# 	set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY ${COTER_OUTPUT_DIR})					
-# endif()
+set(CMAKE_C_STANDARD 99)
+set(CMAKE_C_STANDARD_REQUIRED ON)
+set(CMAKE_C_EXTENSIONS ON)
+set(CMAKE_CXX_STANDARD 11)
+set(CMAKE_CXX_STANDARD_REQUIRED ON)
+set(CMAKE_CXX_EXTENSIONS ON)
+set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
+set(CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS ON)
+set(CMAKE_POSITION_INDEPENDENT_CODE ON)
 
 include(GNUInstallDirs)
 set(CMAKE_SKIP_BUILD_RPATH OFF)
@@ -42,37 +27,67 @@ if(PROJECT_IS_TOP_LEVEL)
 	endif()
 endif()
 
-set(CMAKE_C_STANDARD 99)
-set(CMAKE_C_STANDARD_REQUIRED ON)
-set(CMAKE_C_EXTENSIONS ON)
-set(CMAKE_CXX_STANDARD 11)
-set(CMAKE_CXX_STANDARD_REQUIRED ON)
-set(CMAKE_CXX_EXTENSIONS ON)
-set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
-set(CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS ON)
-set(CMAKE_POSITION_INDEPENDENT_CODE ON)
+add_library(${PROJECT_NAME}_compile_dependency INTERFACE)
+target_compile_features(${PROJECT_NAME}_compile_dependency INTERFACE c_std_99)
 
-# 为MSVC编译器设置特定的C语言选项
+if(NOT DEFINED ENV{CPM_SOURCE_CACHE})
+	set(ENV{CPM_SOURCE_CACHE} ${CMAKE_SOURCE_DIR}/cmake_modules)
+endif()
+string(TOLOWER "${CMAKE_SYSTEM_NAME}" SYSTEM_NAME)
+if(PROJECT_BUILD_DEBUG)
+	set(PROJECT_OUTPUT_DIR ${PROJECT_SOURCE_DIR}/bin/debug-${SYSTEM_NAME})
+	target_compile_definitions(${PROJECT_NAME}_compile_dependency INTERFACE -DDEBUG)
+else()
+	set(PROJECT_OUTPUT_DIR ${PROJECT_SOURCE_DIR}/bin/release-${SYSTEM_NAME})
+	target_compile_definitions(${PROJECT_NAME}_compile_dependency INTERFACE -DNDEBUG)
+endif()
+
+if(NOT CMAKE_RUNTIME_OUTPUT_DIRECTORY)
+	set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${PROJECT_OUTPUT_DIR})					
+endif()
+if(NOT CMAKE_LIBRARY_OUTPUT_DIRECTORY)
+	set(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${PROJECT_OUTPUT_DIR})					
+endif()
+# if(NOT CMAKE_ARCHIVE_OUTPUT_DIRECTORY)
+# 	set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY ${PROJECT_OUTPUT_DIR})					
+# endif()
+
+# set source charset to utf-8 for MSVC
 if(CMAKE_C_COMPILER_ID STREQUAL "MSVC")
-    target_compile_options(coter_compile_dependency INTERFACE 
+    target_compile_options(${PROJECT_NAME}_compile_dependency INTERFACE 
 		"$<$<COMPILE_LANGUAGE:C>:/source-charset:utf-8>"
 	)
 endif()
+if(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
+    target_compile_options(${PROJECT_NAME}_compile_dependency INTERFACE 
+		"$<$<COMPILE_LANGUAGE:CXX>:/source-charset:utf-8>"
+	)
+endif()
 
-# 为非MSVC和非ClangCL（MSVC前端）的编译器设置C语言警告选项
-# 这些选项有助于捕捉潜在的代码问题
+# compiler warnings, skipped for MSVC & ClangCL (MSVC frontend)
 if(NOT (CMAKE_C_COMPILER_ID STREQUAL "MSVC" OR ("${CMAKE_C_COMPILER_ID}" STREQUAL "Clang" AND "${CMAKE_C_COMPILER_FRONTEND_VARIANT}" STREQUAL "MSVC")))
-    target_compile_options(coter_compile_dependency INTERFACE
+    target_compile_options(${PROJECT_NAME}_compile_dependency INTERFACE
         "$<$<COMPILE_LANGUAGE:C>:-Wall>"
         "$<$<COMPILE_LANGUAGE:C>:-Wextra>"
         "$<$<COMPILE_LANGUAGE:C>:-Werror=return-type>"
     )
 endif()
+if(NOT (CMAKE_CXX_COMPILER_ID STREQUAL "MSVC" OR ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang" AND "${CMAKE_CXX_COMPILER_FRONTEND_VARIANT}" STREQUAL "MSVC")))
+    target_compile_options(${PROJECT_NAME}_compile_dependency INTERFACE
+        "$<$<COMPILE_LANGUAGE:CXX>:-Wall>"
+        "$<$<COMPILE_LANGUAGE:CXX>:-Wextra>"
+        "$<$<COMPILE_LANGUAGE:CXX>:-Werror=return-type>"
+    )
+endif()
 
-# 处理旧版本GCC（低于6.0）的特定警告抑制选项
-# -Wno-missing-field-initializers 用于抑制因结构体未完全初始化而产生的警告
+# Handle -Wno-missing-field-initializers for older GCC
 if(CMAKE_C_COMPILER_ID STREQUAL "GNU" AND CMAKE_C_COMPILER_VERSION VERSION_LESS 6.0)
-    target_compile_options(coter_compile_dependency INTERFACE 
+    target_compile_options(${PROJECT_NAME}_compile_dependency INTERFACE 
 		"$<$<COMPILE_LANGUAGE:C>:-Wno-missing-field-initializers>"
+	)
+endif()
+if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU" AND CMAKE_CXX_COMPILER_VERSION VERSION_LESS 6.0)
+    target_compile_options(${PROJECT_NAME}_compile_dependency INTERFACE 
+		"$<$<COMPILE_LANGUAGE:CXX>:-Wno-missing-field-initializers>"
 	)
 endif()
