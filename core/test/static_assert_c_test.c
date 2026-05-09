26d8d291af12f080e5a3d49bf311dbe5132b4f2d
tayne3 <tayne3@dingtalk.com>
2026-05-10 00:09:57 +0800

    chore: add compile tests for CT_STATIC_ASSERT

---
+++ core/test/static_assert_c_test.c
@@ -0,0 +1,36 @@
+#include "coter/core/macro.h"
+
+struct test_struct {
+    char a;
+    int  b;
+};
+
+CT_STATIC_ASSERT(true);
+CT_STATIC_ASSERT(sizeof(char) == 1);
+CT_STATIC_ASSERT(sizeof(short) >= sizeof(char));
+CT_STATIC_ASSERT(sizeof(int) >= sizeof(short));
+CT_STATIC_ASSERT(sizeof(long) >= sizeof(int));
+CT_STATIC_ASSERT(sizeof(long long) >= sizeof(long));
+CT_STATIC_ASSERT(sizeof(double) >= sizeof(float));
+
+CT_STATIC_ASSERT(OFFSET_OF(struct test_struct, a) == 0);
+CT_STATIC_ASSERT(OFFSET_OF(struct test_struct, b) > OFFSET_OF(struct test_struct, a));
+CT_STATIC_ASSERT(OFFSET_OF(struct test_struct, b) == offsetof(struct test_struct, b));
+
+int main(void) {
+    CT_STATIC_ASSERT(sizeof(uint8_t) == 1);
+    CT_STATIC_ASSERT(sizeof(uint16_t) == 2);
+    CT_STATIC_ASSERT(sizeof(uint32_t) == 4);
+    CT_STATIC_ASSERT(sizeof(uint64_t) == 8);
+
+    CT_STATIC_ASSERT(sizeof(int8_t) == 1);
+    CT_STATIC_ASSERT(sizeof(int16_t) == 2);
+    CT_STATIC_ASSERT(sizeof(int32_t) == 4);
+    CT_STATIC_ASSERT(sizeof(int64_t) == 8);
+
+    CT_STATIC_ASSERT(OFFSET_OF(struct test_struct, a) == 0);
+    CT_STATIC_ASSERT(OFFSET_OF(struct test_struct, b) > OFFSET_OF(struct test_struct, a));
+    CT_STATIC_ASSERT(OFFSET_OF(struct test_struct, b) == offsetof(struct test_struct, b));
+
+    return 0;
+}
