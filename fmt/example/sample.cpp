5991a1b8b0d69f04e0667f0b36bfbe95b7fea59d
tayne3 <tayne3@dingtalk.com>
2026-06-02 09:26:43 +0800

    feat(fmt): vendorize fmt library v12.1.0

---
+++ fmt/example/sample.cpp
@@ -0,0 +1,5 @@
+#include "coter/fmt/base.h"
+
+int main(int argc, char** argv) {
+    for (int i = 0; i < argc; ++i) fmt::print("{}: {}\n", i, argv[i]);
+}
---
+++ fmt/include/coter/fmt/core.h
@@ -0,0 +1,15 @@
+// This file is only provided for compatibility and may be removed in future
+// versions. Use fmt/base.h if you don't need fmt::format and fmt/format.h
+// otherwise.
+
+#ifndef COTER_FMT_FMT_CORE_H_
+#define COTER_FMT_FMT_CORE_H_
+
+#include "coter/core/macro.h"
+
+#if defined(__cplusplus) && CT_CXX_STANDARD >= CT_CXX_11
+
+#include "format.h"
+
+#endif  // CT_CXX_STANDARD >= CT_CXX_11
+#endif  // COTER_FMT_FMT_CORE_H_
