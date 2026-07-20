aa14135a740e2489ebe10e8d4764c7ef1cdef481
tayne3 <tayne3@dingtalk.com>
2026-07-20 09:36:51 +0800

    build: simplify platform configuration checks

--- cmake/coter_platform_config.cmake
+++ cmake/coter_platform_config.cmake
@@ -6,20 +6,26 @@ Checks for and links against required system libraries based on feature
 detection rather than hardcoded OS checks where possible.
 #]=======================================================================]
 
-coter_check_header("inttypes.h")
-coter_check_header("stdbool.h")
-coter_check_header("sys/types.h")
-coter_check_header("sys/stat.h")
+include(CheckIncludeFiles)
+include(CheckSymbolExists)
+include(CheckLibraryExists)
 
-coter_check_function("memrchr" "string.h")
-coter_check_function("clock_gettime" "time.h")
-coter_check_function("sem_timedwait" "semaphore.h")
-coter_check_function("pthread_rwlock_init" "pthread.h")
+check_include_files("inttypes.h" HAVE_INTTYPES_H)
+check_include_files("stdbool.h" HAVE_STDBOOL_H)
+check_include_files("sys/types.h" HAVE_SYS_TYPES_H)
+check_include_files("sys/stat.h" HAVE_SYS_STAT_H)
+
+check_symbol_exists("memrchr" "string.h" HAVE_MEMRCHR)
+check_symbol_exists("clock_gettime" "time.h" HAVE_CLOCK_GETTIME)
+check_symbol_exists("sem_timedwait" "semaphore.h" HAVE_SEM_TIMEDWAIT)
+check_symbol_exists("pthread_rwlock_init" "pthread.h" HAVE_PTHREAD_RWLOCK)
 
 if(UNIX AND NOT APPLE)
-  coter_check_library_function("rt" "clock_gettime" "")
-  if(HAVE_CLOCK_GETTIME_IN_RT)
-    set(HAVE_CLOCK_GETTIME 1)
-    target_link_libraries(coter_internal_options INTERFACE rt)
+  if(NOT HAVE_CLOCK_GETTIME)
+    check_library_exists("rt" "clock_gettime" "" HAVE_CLOCK_GETTIME_IN_RT)
+    if(HAVE_CLOCK_GETTIME_IN_RT)
+      set(HAVE_CLOCK_GETTIME TRUE)
+      target_link_libraries(coter_internal_options INTERFACE rt)
+    endif()
   endif()
 endif()
